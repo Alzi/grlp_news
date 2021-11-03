d449f5bb40634ce66e5edaa300113df6a2e8ffc3
alzi <alzi@uber.space>
2021-11-03 10:59:55 +0100

    Better file-structure; Feature: shortcode-attribute 'spalten'; Fix plugin-description

---
+++ README.md
@@ -0,0 +1,29 @@
+GRLP-News
+=========
+
+Wordpress-Plugin für die Website des Landesverbandes von
+Bündnis 90/Die Grünen Rheinland Pfalz
+
+Das PlugIn fügt einen Shortcode hinzu mit dem eine dreispaltige Ansicht
+der aktuellen Beiträge der Kategorie 'Presse' dargestellt werden kann.
+Die Seiten-Einstellung `Layout selbst gestalten` im __Sunflower__ Theme
+ist sinnvoll, weil dadurch die 3 Spalten etwas mehr Platz haben.
+Das PlugIn wird noch weiterentwickelt.
+
+Anwendung
+---------
+
+	[news titel="Eine optionale Überschrift"]
+
+Versionsverlauf
+---------------
+
+### V 1.1
+
+- Durch das Attribut `spalten` kann die Anzahl der angezeigten Spalten
+  gesteuert werden. (Nur eine Zeile möglich)
+
+### V 1.0
+
+- Eine 3-spaltige Ansicht der aktuellsten Beiträge aus der Kategorie 'Presse'
+- Durch das Attribut `titel` kann einen H1-Überschrift darüber gestellt werden.
--- grlp-news.php
+++ grlp-news.php
@@ -1,11 +1,8 @@
 <?php
 /**
  * Plugin Name: Grlp-News 
- * Description: Fügt einen Shortcode `[news]` hinzu, der es ermöglicht,
- *              eine 3-spaltige Ansicht der neuesten Artikel einer, oder
- *              mehrerer Kategorien anzuzeigen. Die 3 Spalten werden in
- *              gleicher Höhe angezeigt. 
- * Version: 1.0
+ * Description: Fügt einen Shortcode `[news]` hinzu, der es ermöglicht, eine 3-spaltige Ansicht der neuesten Artikel einer, oder mehrerer Kategorien anzuzeigen. Die 3 Spalten werden in gleicher Höhe angezeigt. 
+ * Version: 1.1
  * Author: Marc Dietz 
  * Author URI: mailto:technik@gruene-rlp.de
  * Text Domain: grlp-news 
@@ -23,35 +20,39 @@ function grlp_news_view( $atts, $content, $shortcode_tag )
 {
     $html = '';
     $news_title = '';
+    $num_posts = 3;
+    if ( ! empty( $atts )) {
+        if ( isset( $atts['titel'])) {
+            $news_title = wp_strip_all_tags($atts['titel']);
+        }
+        if ( isset( $atts['spalten'])) {
+            $num_posts = intval( $atts['spalten'] );
+        }
+    }
+
     $query = new WP_Query(
         [
             'post_type' => 'post',
-            'posts_per_page' => 3,
+            'posts_per_page' => $num_posts,
             'category_name' => 'presse',
             'post_status' => 'publish',
         ]
     );
 
-    if ( ! empty( $atts )) {
-        if ( isset( $atts['titel'])) {
-            $news_title = wp_strip_all_tags($atts['titel']);
-        }
-    }
-
     if ($query->have_posts()) {
         ob_start();
-        require ('news_header.php');
+        require ('templates/news_header.php');
         $html .= ob_get_clean();
         while ($query->have_posts()) {
             $query->the_post();
             ob_start();
-            require('news_column.php');
+            require('templates/news_column.php');
             $html .= ob_get_clean();
         }
     }
 
     ob_start();
-    require('news_footer.php');
+    require('templates/news_footer.php');
     $html .= ob_get_clean();
     wp_reset_postdata();
     return $html;
