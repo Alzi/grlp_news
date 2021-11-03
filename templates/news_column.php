121bdf0f64f333d63b60a86de9fc961f0ce4d50b
alzi <alzi@uber.space>
2021-11-03 21:30:05 +0100

    Fix padding between column-header and content (.pb-2)

--- templates/news_column.php
+++ templates/news_column.php
@@ -8,7 +8,7 @@
 
 <div class="wp-block-column p-3 ms-0 ms-sm-3 rounded has-white-background-color has-background has-shadow">
 <article id="post-<?php the_ID(); ?>"class="post">
-    <header class="entry-header">
+    <header class="entry-header pb-2">
         <h2 class="h5 mb-3"><a class="news-title" href="<?php esc_url(the_permalink()); ?>"><?php the_title(); ?></a></h2>
         <div class="entry-meta mb-3"> 
             <span class="posted-on">
